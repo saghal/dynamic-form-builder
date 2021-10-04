6697af253e6aa17d35c55ab568ddeae8a46abbb7
saghal <saghalov@gmail.com>
2021-10-04 16:59:11 +0330

    add routing for formlist

--- dynamic-form-builder/src/app/app-routing.module.ts
+++ dynamic-form-builder/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsListComponent } from './forms-list/forms-list.component';
 import { NewFormComponent } from './new-form/new-form.component';
 
-const routes: Routes = [{ path: '', component: NewFormComponent }];
+const routes: Routes = [
+  { path: '', component: NewFormComponent },
+  { path: 'forms', component: FormsListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
